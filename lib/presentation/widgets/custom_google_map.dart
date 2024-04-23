306070c2851b9334c8e43d3199ef2eac2fbe2bdc
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-04-23 23:20:30 +0200

    Create PolyLines

--- lib/presentation/widgets/custom_google_map.dart
+++ lib/presentation/widgets/custom_google_map.dart
@@ -14,6 +14,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late GoogleMapController googleMapController;
 
   Set<Marker> myMarkers = {};
+  Set<Polyline> myPolyLines = {};
 
   @override
   void initState() {
@@ -23,10 +24,11 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       zoom: 15,
     );
     addMarkers();
+    addPolyLines();
   }
 
-  addMarkers()async {
-    BitmapDescriptor image =await  BitmapDescriptor.fromAssetImage(
+  addMarkers() async {
+    BitmapDescriptor image = await BitmapDescriptor.fromAssetImage(
       const ImageConfiguration(),
       'assets/images/icons8-marker-50.png',
     );
@@ -34,7 +36,8 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     myMarkers.addAll(
       placeModels
           .map(
-            (place) => Marker(
+            (place) =>
+            Marker(
               markerId: MarkerId(place.id.toString()),
               position: place.latLng,
               icon: image,
@@ -42,15 +45,38 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
                 title: place.name,
               ),
             ),
-          )
+      )
           .toSet(),
     );
     setState(() {});
   }
 
+  addPolyLines() {
+    myPolyLines.add(
+       const Polyline(
+        polylineId: PolylineId('1'),
+        width: 2,
+        endCap: Cap.roundCap,
+        // geodesic: true,
+        startCap:Cap.squareCap ,
+        // jointType: JointType.mitered,
+        color: Colors.red,
+        points: [
+          LatLng(31.197765986988546, 29.899822747599988),
+          LatLng(31.201296109318136, 29.91396385665663),
+          LatLng(31.209290311153776, 29.920154385084857),
+          LatLng(31.21505194669606, 29.924099329671478),
+
+        ],
+
+      ),
+    );
+  }
+
   void loadMapStyle() async {
     String mapStyle = await DefaultAssetBundle.of(context)
         .loadString('assets/map_styles/dark_map_style.json');
+
     await googleMapController.setMapStyle(mapStyle);
   }
 
@@ -60,6 +86,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       body: GoogleMap(
         // mapType: MapType.satellite   ,
         initialCameraPosition: initialCameraPosition,
+        polylines:myPolyLines,
         onMapCreated: (controller) {
           googleMapController = controller;
           loadMapStyle();
@@ -69,5 +96,3 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     );
   }
 }
-
-
