2f8d17af217ef1ae0c88553360a857bbb864ec8f
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-04-24 13:12:39 +0200

    check AndRequest Location Service And Permission

--- lib/presentation/widgets/custom_google_map.dart
+++ lib/presentation/widgets/custom_google_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_with_google_maps/data/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -16,6 +17,8 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   Set<Marker> myMarkers = {};
   Set<Polyline> myPolyLines = {};
 
+  late Location location;
+
   @override
   void initState() {
     super.initState();
@@ -23,10 +26,49 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       target: LatLng(31.20632194391642, 29.911010868194964),
       zoom: 15,
     );
-    addMarkers();
-    addPolyLines();
+    // addMarkers();
+    // addPolyLines();
+    location = Location();
+    checkAndRequestLocationServiceAndPermission();
   }
 
+  ///First Step .
+  ///Check  Location Service on Mobile
+  //if(its Not enabled , Ask For enable )
+  //if user Denied -> Show snakbar error
+
+  ///Second Step
+  ///Check  Location Permission on Application
+  // if - he has a Permission
+  //   Check if user Denied For one time -> Ask for permission
+  // if user not accepted again -> Not Granted (denied or denied forever )
+  //Show error
+
+  Future<void> checkAndRequestLocationService() async {
+    bool isEnabled = await location.serviceEnabled();
+    if (!isEnabled) {
+      isEnabled = await location.requestService();
+      if (!isEnabled) {
+        //ToDO: Show Error Bar (Error , OR Don't Have Access ) ;
+      }
+    }
+  }
+
+  Future<void> checkAndRequestLocationPermission() async {
+    PermissionStatus permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+    }
+    if (permissionStatus != PermissionStatus.granted) {
+      ///ToDO: Show Error Bar (Error , OR Don't Have Access ) ;
+    }
+  }
+
+  void checkAndRequestLocationServiceAndPermission()async
+  {
+    await checkAndRequestLocationService();
+    await checkAndRequestLocationPermission();
+  }
   addMarkers() async {
     BitmapDescriptor image = await BitmapDescriptor.fromAssetImage(
       const ImageConfiguration(),
@@ -36,8 +78,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     myMarkers.addAll(
       placeModels
           .map(
-            (place) =>
-            Marker(
+            (place) => Marker(
               markerId: MarkerId(place.id.toString()),
               position: place.latLng,
               icon: image,
@@ -45,7 +86,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
                 title: place.name,
               ),
             ),
-      )
+          )
           .toSet(),
     );
     setState(() {});
@@ -53,12 +94,12 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
   addPolyLines() {
     myPolyLines.add(
-       const Polyline(
+      const Polyline(
         polylineId: PolylineId('1'),
         width: 2,
         endCap: Cap.roundCap,
         // geodesic: true,
-        startCap:Cap.squareCap ,
+        startCap: Cap.squareCap,
         // jointType: JointType.mitered,
         color: Colors.red,
         points: [
@@ -66,9 +107,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
           LatLng(31.201296109318136, 29.91396385665663),
           LatLng(31.209290311153776, 29.920154385084857),
           LatLng(31.21505194669606, 29.924099329671478),
-
         ],
-
       ),
     );
   }
@@ -86,7 +125,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       body: GoogleMap(
         // mapType: MapType.satellite   ,
         initialCameraPosition: initialCameraPosition,
-        polylines:myPolyLines,
+        polylines: myPolyLines,
         onMapCreated: (controller) {
           googleMapController = controller;
           loadMapStyle();
