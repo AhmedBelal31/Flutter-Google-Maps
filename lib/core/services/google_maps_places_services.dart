d9b9600c5329f107cba748d95dc350e0a1434f9e
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-04-28 23:36:36 +0300

    Create Get Predications Method

---
+++ lib/core/services/google_maps_places_services.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:flutter_with_google_maps/data/models/place_autocomplete_model.dart';
+import 'package:http/http.dart' as http;
+import '../utils/api_key.dart';
+
+class GoogleMapsPlacesServices {
+  Future<List<PredictionsModel>> getPredications(
+      {required String input}) async {
+    String baseUrl = 'https://maps.googleapis.com/maps/api/place';
+
+    var response = await http
+        .get(Uri.parse('$baseUrl/autocomplete/json?input=$input&key=$apiKey'));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['predictions'];
+      // List<PredictionsModel> places = [];
+      // for (var placeItem in data) {
+      //   places.add(PredictionsModel.fromJson(placeItem));
+      // }
+      return data.map((placeItem) => PredictionsModel.fromJson(placeItem));
+    } else {
+      throw Exception();
+    }
+  }
+}
